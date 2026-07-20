acaecd83d09f1e882b94b9451034af22e612ea96
HILLOL PAL <hillol-pal@users.noreply.github.com>
2026-07-20 21:47:22 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,89 @@
+
+
+class PrefixTree {
+
+    TrieNode root = null;
+
+    public PrefixTree() {
+
+        root = new TrieNode();
+         
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+
+        for(Character c: word.toCharArray()){
+
+            if(!node.children.containsKey(c)){
+               node.children.put(c,new TrieNode());
+            }
+
+            node = node.children.get(c);
+        }
+
+        node.isWord = true;
+
+    }
+
+    public boolean search(String word) {
+
+        TrieNode node = root;
+
+        for(Character c: word.toCharArray()){
+
+            if(!node.children.containsKey(c)){
+               return false;
+            }
+
+            node = node.children.get(c);
+        }
+
+        if(node.isWord == true)
+          return true;
+
+        return false;
+
+    }
+
+    public boolean startsWith(String prefix) {
+
+        TrieNode node = root;
+
+        for(Character c: prefix.toCharArray()){
+
+            if(!node.children.containsKey(c)){
+               return false;
+            }
+
+            node = node.children.get(c);
+        }
+
+        return true;
+
+    }
+
+    public boolean searchPrefix(String prefix) {
+
+       TrieNode node = root;
+
+        for(Character c: prefix.toCharArray()){
+
+            if(!node.children.containsKey(c)){
+               return false;
+            }
+
+            node = node.children.get(c);
+        }
+
+        return true;
+    }
+
+}
+
+class TrieNode{
+   
+   Map<Character, TrieNode> children = new HashMap<>();
+   boolean isWord = false;
+
+}
